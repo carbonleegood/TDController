17e43ae955ef21bd144aeffcad4a1c4f2eb1bcc7
carbonleegood <408983934@qq.com>
2015-06-24 16:42:29 +0800

    增加技能类

---
+++ Controller/Skill.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Controller
+{
+    //enum SkillType
+    //{
+
+    //}
+    ////基本技能信息
+    //class SingleSkill
+    //{
+    //    //名称
+    //    string name;
+    //    //类型
+    //    SkillType type;
+    //    //技能按键
+    //    int key;
+    //    //释放距离
+    //    int cast_dis;
+    //    //CD时间
+    //}
+    //class SkillCastStep
+    //{
+    //    //技能释放步骤
+    //    void Cast()
+    //    {
+    //        //都需要干什么 
+    //    }
+    //}
+    ////组合技能
+    //class GroupSkill
+    //{
+    //    int ID;//判断单体技能是否可放
+
+    //    List<int> SkillCastStep;
+    //    int Steps { get; set; }
+    //    public void Cast(int step);
+    //}
+
+    ////所有职业技能列表
+    //class ProfessionSkill
+    //{
+    //    List<SingleSkill> AllSkill;
+
+    //    void InitProfessionSkill();
+    //    void UpdateSkill();
+    //    GroupSkill GetSkill();
+    //}
+}
