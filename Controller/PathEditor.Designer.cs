7cc6ced7eb450fb8c259bf3a7b8a8305ea082e35
408983934@qq.com <408983934@qq.com>
2015-06-29 16:16:58 +0800

    完善路径功能

--- Controller/PathEditor.Designer.cs
+++ Controller/PathEditor.Designer.cs
@@ -33,34 +33,46 @@
             this.btnSavePath = new System.Windows.Forms.Button();
             this.btnStopGenPath = new System.Windows.Forms.Button();
             this.btnStartGenPath = new System.Windows.Forms.Button();
+            this.btnReversePath = new System.Windows.Forms.Button();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.btnStopMove = new System.Windows.Forms.Button();
+            this.dgbPath = new System.Windows.Forms.DataGridView();
+            this.x = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnClearPath = new System.Windows.Forms.Button();
+            this.tbPathName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgbPath)).BeginInit();
             this.SuspendLayout();
             // 
             // btnDelPoint
             // 
-            this.btnDelPoint.Location = new System.Drawing.Point(26, 184);
+            this.btnDelPoint.Location = new System.Drawing.Point(26, 237);
             this.btnDelPoint.Name = "btnDelPoint";
             this.btnDelPoint.Size = new System.Drawing.Size(75, 23);
             this.btnDelPoint.TabIndex = 9;
             this.btnDelPoint.Text = "删除点";
             this.btnDelPoint.UseVisualStyleBackColor = true;
+            this.btnDelPoint.Click += new System.EventHandler(this.btnDelPoint_Click);
             // 
             // btnLoadPath
             // 
-            this.btnLoadPath.Location = new System.Drawing.Point(26, 106);
+            this.btnLoadPath.Location = new System.Drawing.Point(26, 124);
             this.btnLoadPath.Name = "btnLoadPath";
             this.btnLoadPath.Size = new System.Drawing.Size(75, 23);
             this.btnLoadPath.TabIndex = 8;
             this.btnLoadPath.Text = "加载路径";
             this.btnLoadPath.UseVisualStyleBackColor = true;
+            this.btnLoadPath.Click += new System.EventHandler(this.btnLoadPath_Click);
             // 
             // btnSavePath
             // 
-            this.btnSavePath.Location = new System.Drawing.Point(26, 135);
+            this.btnSavePath.Location = new System.Drawing.Point(26, 153);
             this.btnSavePath.Name = "btnSavePath";
             this.btnSavePath.Size = new System.Drawing.Size(75, 23);
             this.btnSavePath.TabIndex = 7;
             this.btnSavePath.Text = "保存路径";
             this.btnSavePath.UseVisualStyleBackColor = true;
+            this.btnSavePath.Click += new System.EventHandler(this.btnSavePath_Click);
             // 
             // btnStopGenPath
             // 
@@ -70,6 +82,7 @@
             this.btnStopGenPath.TabIndex = 6;
             this.btnStopGenPath.Text = "停止采集";
             this.btnStopGenPath.UseVisualStyleBackColor = true;
+            this.btnStopGenPath.Click += new System.EventHandler(this.btnStopGenPath_Click);
             // 
             // btnStartGenPath
             // 
@@ -79,12 +92,91 @@
             this.btnStartGenPath.TabIndex = 5;
             this.btnStartGenPath.Text = "开始采集";
             this.btnStartGenPath.UseVisualStyleBackColor = true;
+            this.btnStartGenPath.Click += new System.EventHandler(this.btnStartGenPath_Click);
+            // 
+            // btnReversePath
+            // 
+            this.btnReversePath.Location = new System.Drawing.Point(26, 266);
+            this.btnReversePath.Name = "btnReversePath";
+            this.btnReversePath.Size = new System.Drawing.Size(75, 23);
+            this.btnReversePath.TabIndex = 10;
+            this.btnReversePath.Text = "反转路径";
+            this.btnReversePath.UseVisualStyleBackColor = true;
+            this.btnReversePath.Click += new System.EventHandler(this.btnReversePath_Click);
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(26, 311);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 11;
+            this.btnMove.Text = "走路径";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // btnStopMove
+            // 
+            this.btnStopMove.Location = new System.Drawing.Point(26, 340);
+            this.btnStopMove.Name = "btnStopMove";
+            this.btnStopMove.Size = new System.Drawing.Size(75, 23);
+            this.btnStopMove.TabIndex = 12;
+            this.btnStopMove.Text = "停止走路径";
+            this.btnStopMove.UseVisualStyleBackColor = true;
+            this.btnStopMove.Click += new System.EventHandler(this.btnStopMove_Click);
+            // 
+            // dgbPath
+            // 
+            this.dgbPath.AllowUserToAddRows = false;
+            this.dgbPath.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgbPath.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.x,
+            this.y});
+            this.dgbPath.Location = new System.Drawing.Point(144, 23);
+            this.dgbPath.Name = "dgbPath";
+            this.dgbPath.RowTemplate.Height = 23;
+            this.dgbPath.Size = new System.Drawing.Size(444, 343);
+            this.dgbPath.TabIndex = 13;
+            // 
+            // x
+            // 
+            this.x.HeaderText = "x";
+            this.x.Name = "x";
+            this.x.Width = 200;
+            // 
+            // y
+            // 
+            this.y.HeaderText = "y";
+            this.y.Name = "y";
+            this.y.Width = 200;
+            // 
+            // btnClearPath
+            // 
+            this.btnClearPath.Location = new System.Drawing.Point(26, 208);
+            this.btnClearPath.Name = "btnClearPath";
+            this.btnClearPath.Size = new System.Drawing.Size(75, 23);
+            this.btnClearPath.TabIndex = 14;
+            this.btnClearPath.Text = "清空路径";
+            this.btnClearPath.UseVisualStyleBackColor = true;
+            this.btnClearPath.Click += new System.EventHandler(this.btnClearPath_Click);
+            // 
+            // tbPathName
+            // 
+            this.tbPathName.Location = new System.Drawing.Point(26, 97);
+            this.tbPathName.Name = "tbPathName";
+            this.tbPathName.Size = new System.Drawing.Size(75, 21);
+            this.tbPathName.TabIndex = 15;
             // 
             // PathEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(569, 314);
+            this.ClientSize = new System.Drawing.Size(635, 396);
+            this.Controls.Add(this.tbPathName);
+            this.Controls.Add(this.btnClearPath);
+            this.Controls.Add(this.dgbPath);
+            this.Controls.Add(this.btnStopMove);
+            this.Controls.Add(this.btnMove);
+            this.Controls.Add(this.btnReversePath);
             this.Controls.Add(this.btnDelPoint);
             this.Controls.Add(this.btnLoadPath);
             this.Controls.Add(this.btnSavePath);
@@ -92,7 +184,9 @@
             this.Controls.Add(this.btnStartGenPath);
             this.Name = "PathEditor";
             this.Text = "PathEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgbPath)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -103,5 +197,13 @@
         private System.Windows.Forms.Button btnSavePath;
         private System.Windows.Forms.Button btnStopGenPath;
         private System.Windows.Forms.Button btnStartGenPath;
+        private System.Windows.Forms.Button btnReversePath;
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Button btnStopMove;
+        private System.Windows.Forms.DataGridView dgbPath;
+        private System.Windows.Forms.Button btnClearPath;
+        private System.Windows.Forms.DataGridViewTextBoxColumn x;
+        private System.Windows.Forms.DataGridViewTextBoxColumn y;
+        private System.Windows.Forms.TextBox tbPathName;
     }
 }
--- Controller/PathEditor.cs
+++ Controller/PathEditor.cs
@@ -6,14 +6,219 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using Thrift.GameCall;
+using System.Threading;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 namespace Controller
 {
     public partial class PathEditor : Form
     {
+        bool bWorking = false;
         public PathEditor()
         {
             InitializeComponent();
         }
+        delegate void F_AddPoint(double x, double y);
+        void GenPathThread()
+        {
+            const double StepDis = 300.0f;
+            F_AddPoint addPoint = new F_AddPoint(AddPoint);
+            PosInfo LastSavePos=null;
+            while(bWorking)
+            {
+                PosInfo pPos=Program.client.GetPlayerPos();
+                double dis = StepDis;
+                if (LastSavePos != null)
+                    dis = Func.GetDis(pPos.X, pPos.Y, LastSavePos.X, LastSavePos.Y);
+                if(dis<StepDis)
+                {
+                    Thread.Sleep(200);
+                    continue;
+                }
+                LastSavePos = pPos;
+                addPoint(pPos.X, pPos.Y);
+                Thread.Sleep(500);
+            }
+        }
+        void AddPoint(double x, double y)
+        {
+            int nRow = dgbPath.Rows.Add();
+         //   dgbPath.Rows[nRow].Cells[0].Value = nRow.ToString();
+            dgbPath.Rows[nRow].Cells[0].Value = x.ToString();
+            dgbPath.Rows[nRow].Cells[1].Value = y.ToString();
+        }
+        private void btnStartGenPath_Click(object sender, EventArgs e)
+        {
+            if(bWorking==true)
+                return;
+            bWorking=true;
+            Thread t = new Thread(GenPathThread);
+            t.IsBackground = false;
+            t.Start();
+        }
+
+        private void btnStopGenPath_Click(object sender, EventArgs e)
+        {
+            bWorking = false;
+        }
+
+        private void btnLoadPath_Click(object sender, EventArgs e)
+        {
+            dgbPath.Rows.Clear();
+
+            //string filename = @"aaaa.path";
+            string filename = tbPathName.Text;
+            if (filename.Length < 1)
+            {
+                MessageBox.Show("请输入路径文件名");
+                return;
+            }
+            filename += ".path";
+
+            Stream fStream = null;
+            try
+            {
+                fStream = new FileStream(filename, FileMode.Open, FileAccess.ReadWrite);
+            }
+            catch (Exception err)
+            {
+                return;
+            }
+            int n = 0;
+            BinaryFormatter binFormat = new BinaryFormatter();
+            while (true)
+            {
+                try
+                {
+                    PathPoint point = (PathPoint)binFormat.Deserialize(fStream);
+                    int nRow = dgbPath.Rows.Add();
+                    dgbPath.Rows[nRow].Cells[0].Value = point.x.ToString();
+                    dgbPath.Rows[nRow].Cells[1].Value = point.y.ToString();
+
+                }
+                catch (Exception err)
+                {
+                    break;
+                }
+                ++n;
+            }
+            fStream.Close();
+        }
+
+        private void btnSavePath_Click(object sender, EventArgs e)
+        {
+            string filename = tbPathName.Text;
+            if (filename.Length < 1)
+            {
+                MessageBox.Show("请输入路径文件名");
+                return;
+            }
+            filename += ".path";
+
+            Stream fStream = null;
+            fStream = new FileStream(filename, FileMode.Create, FileAccess.ReadWrite);
+            BinaryFormatter binFormat = new BinaryFormatter();
+            PathPoint point = new PathPoint();
+            for (int i = 0; i < dgbPath.Rows.Count; ++i)
+            {
+                
+                double.TryParse(dgbPath.Rows[i].Cells[0].Value.ToString(), out point.x);
+                double.TryParse(dgbPath.Rows[i].Cells[1].Value.ToString(), out point.y);
+                binFormat.Serialize(fStream, point);
+            }
+            fStream.Close();
+        }
+
+        private void btnDelPoint_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dgbPath.Rows.Remove(dgbPath.CurrentRow);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.ToString());
+            }
+        }
+        const double ArriveDis = 100.0f;
+        void MoveThread()
+        {
+            //获取路径
+            List<PathPoint> path = new List<PathPoint>();
+            for (int i = 0; i < dgbPath.Rows.Count; ++i)
+            {
+                PathPoint point = new PathPoint();
+                double.TryParse(dgbPath.Rows[i].Cells[0].Value.ToString(), out point.x);
+                double.TryParse(dgbPath.Rows[i].Cells[1].Value.ToString(), out point.y);
+                path.Add(point);
+            }
+
+            int TargetPointNum=0;
+            Program.client.PressKey(87, 1);//开始走
+            while(bWorking)
+            {
+                //获取角色位置
+                PosInfo pPos = Program.client.GetPlayerPos();
+                //计算距离,小于XX则切换下一个点,到达终点则退出
+                double dis=Func.GetDis(path[TargetPointNum].x, path[TargetPointNum].y, pPos.X, pPos.Y);
+                if(dis<ArriveDis)
+                {
+                    ++TargetPointNum;
+                    if(TargetPointNum>=path.Count)
+                    {
+                        Program.client.PressKey(87,1);
+                        bWorking = false;
+                        break;
+                    }
+                }
+                //调整面向角
+                double angle = Func.CalcAngle(pPos.X, pPos.Y, path[TargetPointNum].x, path[TargetPointNum].y);
+                Program.client.ChangeAngle(angle);
+                Thread.Sleep(200);
+            }
+        }
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            AddPoint(100, 200);
+        }
+
+        private void btnStopMove_Click(object sender, EventArgs e)
+        {
+            bWorking = false;
+        }
+
+        private void btnReversePath_Click(object sender, EventArgs e)
+        {
+            List<PathPoint> path = new List<PathPoint>();
+            for(int i=0;i<dgbPath.Rows.Count;++i)
+            {
+                PathPoint point = new PathPoint();
+                double.TryParse(dgbPath.Rows[i].Cells[0].Value.ToString(), out point.x);
+                double.TryParse(dgbPath.Rows[i].Cells[1].Value.ToString(), out point.y);
+                path.Add(point);
+            }
+
+            path.Reverse();
+            dgbPath.Rows.Clear();
+            foreach(var item in path)
+            {
+                int nRow = dgbPath.Rows.Add();
+                dgbPath.Rows[nRow].Cells[0].Value = item.x.ToString();
+                dgbPath.Rows[nRow].Cells[1].Value = item.y.ToString();
+            }
+
+        }
+
+        private void btnClearPath_Click(object sender, EventArgs e)
+        {
+            dgbPath.Rows.Clear();
+        }
+    }
+    [Serializable]
+    class PathPoint
+    {
+        public double x=0;
+        public double y=0;
     }
 }
